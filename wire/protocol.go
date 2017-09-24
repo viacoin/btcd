a8fd434438e2e9491c6946a80eadc8aeb047f067
romanornr <romanornr@gmail.com>
2017-09-24 18:41:07 +0200

    Viacoin: [wire] magicbytes mainnet

--- wire/protocol.go
+++ wire/protocol.go
@@ -130,7 +130,7 @@ type BitcoinNet uint32
 // better idea to simply disconnect clients that are misbehaving over TCP.
 const (
 	// MainNet represents the main bitcoin network.
-	MainNet BitcoinNet = 0xd9b4bef9
+	MainNet BitcoinNet = 0xcbc6680f
 
 	// TestNet represents the regression test network.
 	TestNet BitcoinNet = 0xdab5bffa
